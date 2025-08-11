24f47b98846a4e3d7ca70b8d7f6236fd73ea20da
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2025-08-11 20:28:09 -0300

    resolvendo os exercicios

---
+++ src/main/java/br/com/vasconcelos/controllers/MathController.java
@@ -0,0 +1,100 @@
+package br.com.vasconcelos.controllers;
+
+
+import br.com.vasconcelos.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/math")
+public class MathController {
+
+
+   @RequestMapping("sum/{numberOne}/{numberTwo}")
+   public Double sum(
+     @PathVariable("numberOne")String numberOne,
+     @PathVariable("numberTwo")     String numberTwo
+   ) throws Exception {
+
+     if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+   return convertToDouble(numberOne) + convertToDouble(numberTwo);
+
+   }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
+        String number = strNumber.replace(",",".");
+        return Double.parseDouble(number);
+   }
+
+
+    private boolean isNumeric(String strNumber){
+
+       if(strNumber == null || strNumber.isEmpty()) return false;
+       String number = strNumber.replace(",",".");
+
+       return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+
+   }
+
+
+
+
+    @RequestMapping("subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable("numberOne")String numberOne,
+            @PathVariable("numberTwo")     String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+
+    }
+
+
+    @RequestMapping("division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable("numberOne")String numberOne,
+            @PathVariable("numberTwo")     String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+
+    }
+
+    @RequestMapping("multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable("numberOne")String numberOne,
+            @PathVariable("numberTwo")     String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+
+    }
+
+    @RequestMapping("median/{numberOne}/{numberTwo}")
+    public Double median(
+            @PathVariable("numberOne")String numberOne,
+            @PathVariable("numberTwo")     String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+
+    }
+
+
+    @RequestMapping("squareRoot/{numberOne}/{numberTwo}")
+    public Double squareRoot(
+            @PathVariable("numberOne")String numberOne,
+            @PathVariable("numberTwo")     String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))  throw new UnsupportedMathOperationException("Please set a numeric value");
+        return Math.sqrt(convertToDouble(numberOne) + convertToDouble(numberTwo));
+
+    }
+}
---
+++ src/main/java/br/com/vasconcelos/exceptions/hadler/CustomEntityResponseHadler.java
@@ -0,0 +1,55 @@
+package br.com.vasconcelos.exceptions.hadler;
+
+
+import br.com.vasconcelos.exceptions.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class CustomEntityResponseHadler extends ResponseEntityExceptionHandler {
+
+
+
+ @ExceptionHandler(Exception.class)
+ public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+
+     ExceptionResponse response = new ExceptionResponse(
+             new Date(),
+             ex.getMessage(),
+             request.getDescription(false   ));
+
+          return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+
+
+
+
+
+ }
+
+ @ExceptionHandler(UnsupportedOperationException.class)
+ public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+
+     ExceptionResponse response = new ExceptionResponse(
+             new Date(),
+             ex.getMessage(),
+             request.getDescription(false   ));
+
+          return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+
+
+
+
+ }
+
+
+
+}
